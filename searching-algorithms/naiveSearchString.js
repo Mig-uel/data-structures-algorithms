04dd59b0709fef66dd70411a2f03b995bcd6752d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-03-01 08:21:47 -0500

    Add naive string search implementation in naiveSearchString.js
    - Introduces a naive search algorithm for string matching
    - Allows counting occurrences of a substring within a longer string

---
+++ searching-algorithms/naiveSearchString.js
@@ -0,0 +1,19 @@
+/**
+ * Naive Search String
+ */
+
+function naiveSearchString(long, short) {
+  let counter = 0
+
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break
+
+      if (j === short.length - 1) counter++
+    }
+  }
+
+  return counter
+}
+
+console.log(naiveSearchString('wowomgzomg', 'omg'))
